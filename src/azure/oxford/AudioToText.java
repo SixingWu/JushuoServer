799d365f4aa8bc1b8a85a8f500be5415fb1277b1
mebiuw <wusixing@pku.edu.cn>
2016-05-13 23:21:52 +0800

    Test

---
+++ src/azure/oxford/AudioToText.java
@@ -0,0 +1,21 @@
+package azure.oxford;
+
+/**
+ * Created by MebiuW on 16/5/13.
+ *
+ * 调用Oxford API,把语音转变成对应的
+ */
+public class AudioToText {
+    /**
+     * TODO 罗霄:给定一个音频地址的URL,调用Oxford,获得相应识别后的文本,Oxford的API请使用Configuration里的API
+     * @param audioUrl
+     * @return
+     */
+    public static String request(String audioUrl){
+        return null;
+    }
+
+    public static void main(String args[]){
+        //TODO 在这里给我留一个例子我检查一下
+    }
+}
---
+++ src/azure/oxford/Configuration.java
@@ -0,0 +1,10 @@
+package azure.oxford;
+
+/**
+ * Created by MebiuW on 16/5/13.
+ */
+public class Configuration{
+/**
+ * TODO 请将Oxford用到的一些参数,用public static final String XXX的形式放在这里
+ */
+}
