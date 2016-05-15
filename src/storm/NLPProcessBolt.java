d4a3f8fc6dd5ac10d1e78804622c6d89ab5dcccc
mebiuw <wusixing@pku.edu.cn>
2016-05-15 08:48:08 +0800

    +Storm Architecture

---
+++ src/storm/NLPProcessBolt.java
@@ -0,0 +1,30 @@
+package storm;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+
+import java.util.Map;
+
+/**
+ * 自然语言处理
+ * Created by MebiuW on 16/5/15.
+ */
+public class NLPProcessBolt extends BaseRichBolt {
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+
+    }
+}
